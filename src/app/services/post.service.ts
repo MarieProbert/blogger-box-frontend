b9c231905f1286f17128a90b4d46ba72b174c4f0
Marie Probert <124511456+MarieProbert@users.noreply.github.com>
2025-04-11 10:46:42 +0200

    Hands on frontend

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
-import { POSTS, Post } from '../data/post';
+import { Post } from '../data/post';
+import { environment } from '../environment/environment';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class PostService {
-    constructor(private http: HttpClient) { }
+  private baseUrl = `${environment.apiUrl}v1/posts`;
 
-    getPosts(): Observable<Post[]> {
-        const posts = of(POSTS);
-        return posts;
-    }
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.baseUrl}`);
+  }
+
+  getPostById(id: string): Observable<Post> {
+    return this.http.get<Post>(`${this.baseUrl}/${id}`);
+  }
+
+  getPostsByCategoryId(categoryId: string): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.baseUrl}/category/${categoryId}`);
+  }
+
+  createPost(post: Post): Observable<void> {
+    return this.http.post<void>(`${this.baseUrl}`, post);
+  }
+
+  updatePost(id: string, post: Post): Observable<void> {
+    return this.http.put<void>(`${this.baseUrl}/${id}`, post);
+  }
+
+  deletePost(id: string): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
 }
