cdd60c6ed5e2bc44103e5297fbd262faf83368d4
Marie Probert <124511456+MarieProbert@users.noreply.github.com>
2025-04-11 09:40:29 +0200

    Angular getting started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
-
+import { provideHttpClient } from '@angular/common/http';
+import { PostService } from './services/post.service';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
+    PostListComponent,
+    PostListItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(),
+    PostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
